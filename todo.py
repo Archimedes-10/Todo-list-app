77d84bbe35b6ce202e44c0bdbd3b30c6515c378a
Archimedes-10 <marie.wright@ascte.org>
2025-08-29 11:26:09 -0500

    Update todo.py

--- todo.py
+++ todo.py
@@ -8,19 +8,21 @@ def add_task(task):
     tasks.append(task)
 
 # Step 3: View tasks
-    def view_tasks():
-        for i, task in enumerate(tasks, start=1):
-            print(f"{i}. {task}")
+def view_tasks():
+    for i, task in enumerate(tasks, start=1):
+        print(f"{i}. {task}")
 # Step 4: Delete a task
-    def delete_task2(index):
-        tasks.pop(index)
-        print(f"Deleted: {task}")
+
+#def delete_task2(index):
+    #tasks.pop(index)
+    #print(f"Deleted: {tasks}")
     
-    #def delete_task():
-        #finished_task = input("Is task complete? y or n")
-        #if finished_task == "y":
-            #task_number = input("what tak did you finish")
-            #task.pop(task_number)
+def delete_task(index):
+    finished_task = input("Is task complete? y or n")
+    if finished_task == "y":
+        task_number = input("what task did you finish")
+        tasks.pop(int(task_number))
+        print("Finished Task: " + task_number)
 
 
 
@@ -35,7 +37,10 @@ def add_task(task):
 if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
+    add_task("Push to github")
+    add_task("Test delete_task")
     view_tasks()
-    mark_complete(0)
+    delete_task(1)
+    #mark_complete(0)
     view_tasks()
-    save_tasks()
+    #save_tasks()
